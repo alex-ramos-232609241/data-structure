7beca09678ca7904932d16be150071f7d717f833
scorpionletal <uni.ramos.telecomun@gmail.com>
2024-11-19 23:02:38 -0500

    data structure

---
+++ single-linked-list.py
@@ -0,0 +1,29 @@
+class Single_linked_list:
+    class _Node:
+        def __init__(self, value) -> None:
+            self.value = value
+            self.node_next = None
+    
+    def __init__(self) -> None:
+        self.head = None
+        self.queue = None
+        self.size = 0
+    
+    def __str__(self) -> str:
+        arr = []
+        current_node = self.head
+        while current_node != None:
+            arr.append(current_node.value)
+            current_node = current_node.node_next
+        return str(arr) + "size: " + str(self.size)
+
+    def append(self, value) -> None:
+        node_new = self.Node(value)
+        if self.head == None and self.queue == None:
+            self.head = node_new
+            self.queue = node_new
+        else:
+            self.queue.node_next = node_new
+            self.queue = node_new
+        self.size += 1
+    
